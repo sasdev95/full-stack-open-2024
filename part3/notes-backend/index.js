025beaf0a92a7e4179dca700b0cff68a8f34e5cc
Devon Sasaki <dsasaki3@gatech.edu>
2024-05-27 07:40:57 -1000

    Update notes-backend

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
+
 
 let notes = [
     {
@@ -28,7 +28,10 @@ const requestLogger = (req, res, next) => {
     next()
 }
 
+const cors = require('cors')
+
 app.use(cors())
+
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
