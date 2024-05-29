f855a0962c0e0392446851ab4eaf46c0bf79b817
Devon Sasaki <dsasaki3@gatech.edu>
2024-05-28 22:09:04 -1000

    Add data validation

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,98 +1,120 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const morgan = require('morgan')
 
-app.use(express.static('dist'))
-app.use(express.json())
+const Person = require('./models/person')
 
-morgan.token('body', (req, res) => {
-    if (req.method === 'POST') {
-        return JSON.stringify(req.body)
+app.use(express.static('dist'))
+
+morgan.token('body', (request, response) => {
+    if (request.method === 'POST') {
+        return JSON.stringify(request.body)
     }
     return ''
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
-]
+
+    next(error)
+} 
 
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.json())
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.get('/api/persons', (request, response) => {
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
-    const { name, number } = req.body
+app.post('/api/persons', (request, response, next) => {
+    const { name, number } = request.body
 
-    if (!name || !number) {
+    if (name === undefined || number === undefined) {
         return res.status(400).json({ error: 'content missing' })
     }
 
+    /*
     if (persons.find(p => p.name === name)) {
         return res.status(400).json({ error: 'name must be unique' })
     }
+    */
 
-    const id = Math.floor(Math.random() * 10000)
-    const person = {
-        id: id,
-        name: name,
-        number: number
-    }
-
-    persons = persons.concat(person)
-    res.json(person)
+    const person = new Person({ name, number })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+         })
+         .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-    res.status(204).end()
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
+
+    Person.findByIdAndUpdate(
+        request.params.id, 
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }
+    )
+        .then(updatedPerson => {
+            if (updatedPerson) {
+                response.json(updatedPerson)
+            } else {
+                response.status(404).send({ error: 'Person not found' })
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
-    const date = new Date()
-    res.send(`
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${date}</p>
-    `)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+app.get('/info', (request, response, next) => {
+    Person.countDocuments({})
+        .then(count => {
+            const message = `Phonebook has info for ${count} people<br/>${new Date()}`
+            response.send(message)
+        })
+        .catch(error => next(error))
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ part3/phonebook-backend/models/person.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch(error => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+const personSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: { 
+        type: String,
+        required: true,
+        validate: {
+            validator: function(v) {
+                if (v.length < 8) return false;
+
+                return /^\d{2,3}-\d+$/.test(v)
+            }
+        }
+    }
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
