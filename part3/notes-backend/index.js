44710eed1eedb4482703ff349c0cbc39149cb50e
Devon Sasaki <dsasaki3@gatech.edu>
2024-05-28 17:13:08 -1000

    Update index.js

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,7 +1,10 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
+require('dotenv').config()
+
 const Note = require('./models/note')
+
+app.use(express.static('dist'))
 
 const requestLogger = (req, res, next) => {
     console.log('Method:', req.method)
@@ -11,16 +14,26 @@ const requestLogger = (req, res, next) => {
     next()
 }
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+} 
+
 const cors = require('cors')
 
 app.use(cors())
-
-app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
 
-const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.get('/', (request, response) => {
@@ -45,19 +58,9 @@ app.get('/api/notes/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
 app.post('/api/notes', (request, response, next) => {
     const body = request.body
-    if (body.content === undefined) {
-        return response.status(400).json({ error: 'content missing' })
-    }
-
+ 
     const note = new Note({
         content: body.content,
         important: body.important || false,
@@ -72,11 +75,6 @@ app.post('/api/notes', (request, response, next) => {
 
 app.put('/api/notes/:id', (request, response, next) => {
     const { content, important } = request.body
-
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
 
     Note.findByIdAndUpdate(
         request.params.id, 
@@ -98,19 +96,6 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-
-    next(error)
-} 
-
 app.use(errorHandler)
 
 // Bind http server assigned to app variable to listen to HTTP requests sent to port 3001
