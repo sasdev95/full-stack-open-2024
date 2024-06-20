13511a1b5c82008f322d33c27f4eed7cff4dfe8e
Devon Sasaki <dsasaki3@gatech.edu>
2024-06-19 18:13:30 -1000

    Implement Anecdotes app using redux
    -Implemented functionality to vote for anecdotes, create new anecdotes, and save these actions to the Redux store.
    -Ensured anecdotes are displayed in descending order of votes.
    -Refactored code by creating action creator functions and separating logic into AnecdoteForm and AnecdoteList components for improved modularity and readability.

---
+++ part6/redux-anecdotes/src/main.jsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import App from './App'
+import anecdoteReducer from './reducers/anecdoteReducer'
+
+const store = configureStore({ reducer: anecdoteReducer })
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
