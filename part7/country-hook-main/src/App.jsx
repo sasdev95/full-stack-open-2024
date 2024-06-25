431c089a2bedd009376fdbd4888b7fbf8fb3fe6c
Devon Sasaki <dsasaki3@gatech.edu>
2024-06-24 21:45:59 -1000

    Implement custom hooks for country app, notes/phonebook app
    -Developed the useCountry custom hook to dynamically search and retrieve country details from an external API.
    -Abstracted backend communication into a reusable useResource hook that manages data retrieval and creation for various data types like notes and persons.

---
+++ part7/country-hook-main/src/App.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    if (name)
+      axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+        .then(response => {
+          if (response && response.data && response.data.length > 0) {
+            setCountry({ found: true, data: response.data[0] })
+          } else {
+            setCountry({ found: false })
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching country data:', error)
+          setCountry({ found: false })
+        })
+  }, [name])
+
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return <div>not found...</div>
+  }
+  console.log(country)
+  return (
+    <div>
+      <h3>{country.data.name.common} </h3>
+      <div>capital {country.data.capital[0]} </div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.name.common}`}/>  
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
---
+++ part7/ultimate-hooks-main/src/App.jsx
@@ -0,0 +1,99 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const reset = () => {
+    setValue('')
+  }
+
+  return {
+    input: {
+      type,
+      value,
+      onChange
+    },
+    reset
+  }
+}
+
+const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  useEffect(() => {
+    const fetchAll = async () => {
+      try {
+        const response = await axios.get(baseUrl)
+        setResources(response.data)
+      } catch (error) {
+        console.error('Error fetching resources:', error)
+      }
+    }
+    fetchAll()
+  }, [baseUrl])
+
+  const create = async (newResource) => {
+    try {
+      const response = await axios.post(baseUrl, newResource)
+      setResources(prevResources => [...prevResources, response.data])
+    } catch (error) {
+      console.error('Error fetching resource:', error)
+    }
+  }
+
+  const service = {
+    create
+  }
+
+  return [
+    resources, service
+  ]
+}
+
+const App = () => {
+  const content = useField('text')
+  const name = useField('text')
+  const number = useField('text')
+
+  const [notes, noteService] = useResource('http://localhost:3005/notes')
+  const [persons, personService] = useResource('http://localhost:3005/persons')
+
+  const handleNoteSubmit = (event) => {
+    event.preventDefault()
+    noteService.create({ content: content.input.value })
+    content.reset()
+  }
+ 
+  const handlePersonSubmit = (event) => {
+    event.preventDefault()
+    personService.create({ name: name.input.value, number: number.input.value})
+    name.reset()
+    number.reset()
+  }
+
+  return (
+    <div>
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...content.input} />
+        <button>create</button>
+      </form>
+      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input {...name.input} /> <br/>
+        number <input {...number.input} />
+        <button>create</button>
+      </form>
+      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+    </div>
+  )
+}
+
+export default App
